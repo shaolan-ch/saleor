be55307aba0d8abc575612a514159c715d7d00bb
Nicolas <nic.brechet@gmail.com>
2020-10-21 19:45:41 +0200

    test1

--- saleor/plugins/swissvat/__init__.py
+++ saleor/plugins/swissvat/__init__.py
@@ -1,18 +1,1 @@
 
-DEFAULT_TAX_RATE_NAME = "standard"
-
-def get_taxes_for_country(country):
-    taxes = {
-        DEFAULT_TAX_RATE_NAME: {
-            "value": 7,
-            "tax": None,
-        }
-    }
-    
-    return taxes
-
-def get_taxed_shipping_price(shipping_price, taxes):
-    """Calculate shipping price based on settings and taxes."""
-    if not charge_taxes_on_shipping():
-        taxes = None
-    return apply_tax_to_price(taxes, DEFAULT_TAX_RATE_NAME, shipping_price)
--- saleor/plugins/swissvat/plugin.py
+++ saleor/plugins/swissvat/plugin.py
@@ -1,18 +1,6 @@
-from prices import Money, MoneyRange, TaxedMoney, TaxedMoneyRange
+from django.conf import settings
 
-from ...checkout import calculations
-from ...core.taxes import TaxType
-from ...graphql.core.utils.error_codes import PluginErrorCode
-from ...product.models import Product, ProductType
-from ..base_plugin import BasePlugin, ConfigurationTypeField
-from . import (
-    DEFAULT_TAX_RATE_NAME,
-    TaxRateType,
-    apply_tax_to_price,
-    get_taxed_shipping_price,
-    get_taxes_for_country,
-)
-
+from ..base_plugin import BasePlugin
 
 class SwissVatPlugin(BasePlugin):
 
@@ -21,24 +9,4 @@ class SwissVatPlugin(BasePlugin):
     PLUGIN_DESCRIPTION = (
         "Set Swiss Vat"
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.active = True
-
-    def _skip_plugin(self, previous_value: Union[TaxedMoney, TaxedMoneyRange]) -> bool:
-        if not self.active:
-            return True
-
-        # The previous plugin already calculated taxes so we can skip our logic
-        if isinstance(previous_value, TaxedMoneyRange):
-            start = previous_value.start
-            stop = previous_value.stop
-
-            return start.net != start.gross and stop.net != stop.gross
-
-        if isinstance(previous_value, TaxedMoney):
-            return previous_value.net != previous_value.gross
-        return False
-
     
