302d3083abef731245ea76926e244fd66f6553df
Nicolas <nic.brechet@gmail.com>
2020-10-29 00:04:36 +0100

    adds skip

--- saleor/plugins/swissvat/plugin.py
+++ saleor/plugins/swissvat/plugin.py
@@ -9,4 +9,19 @@ class SwissVatPlugin(BasePlugin):
     PLUGIN_DESCRIPTION = (
         "Set Swiss Vat"
     )
+
+    def _skip_plugin(self, previous_value: Union[TaxedMoney, TaxedMoneyRange]) -> bool:
+        if not self.active:
+            return True
+
+        # The previous plugin already calculated taxes so we can skip our logic
+        if isinstance(previous_value, TaxedMoneyRange):
+            start = previous_value.start
+            stop = previous_value.stop
+
+            return start.net != start.gross and stop.net != stop.gross
+
+        if isinstance(previous_value, TaxedMoney):
+            return previous_value.net != previous_value.gross
+        return False
     
