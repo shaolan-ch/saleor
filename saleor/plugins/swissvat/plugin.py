569d126f56847f11c54c9dfaea5ba68b4e75f3a8
Nicolas <nic.brechet@gmail.com>
2020-10-29 01:16:09 +0100

    tax rate

--- saleor/plugins/swissvat/plugin.py
+++ saleor/plugins/swissvat/plugin.py
@@ -2,6 +2,9 @@ from django.conf import settings
 
 from ..base_plugin import BasePlugin
 
+from ...core.taxes import TaxType
+from django_countries.fields import Country
+
 class SwissVatPlugin(BasePlugin):
 
     PLUGIN_NAME = "SwissVat"
@@ -24,4 +27,47 @@ class SwissVatPlugin(BasePlugin):
         if isinstance(previous_value, TaxedMoney):
             return previous_value.net != previous_value.gross
         return False
+    
+    #
+    # Helper methoden
+    #
+    def _get_taxes_for_country(self, country: Country):
+        """Try to fetch cached taxes on the plugin level.
+
+        If the plugin doesn't have cached taxes for a given country it will fetch it
+        from cache or db.
+        """
+        if not country:
+            country = Country(settings.DEFAULT_COUNTRY)
+        country_code = country.code
+        if country_code in self._cached_taxes:
+            return self._cached_taxes[country_code]
+        taxes = get_taxes_for_country(country)
+        self._cached_taxes[country_code] = taxes
+        return taxes
+    
+    #
+    # Main methoden
+    #
+    def get_tax_rate_type_choices(
+        self, previous_value: List["TaxType"]
+    ) -> List["TaxType"]:
+        if not self.active:
+            return previous_value
+
+        choices = [
+            TaxType(code="standard", description="Normale Taxe")
+        ]
+        # sort choices alphabetically by translations
+        return sorted(choices, key=lambda x: x.code)
+
+    def get_tax_rate_percentage_value(
+        self, obj: Union["Product", "ProductType"], country: Country, previous_value
+    ) -> Decimal:
+        """Return tax rate percentage value for given tax rate type in the country."""
+        if not self.active:
+            return previous_value
+
+        return Decimal(7)
+
     
